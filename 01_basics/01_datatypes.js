385facff2fe5f8473ffe3f13e42e4facc2a7e540
kategauransh <127082264+kategauransh@users.noreply.github.com>
2024-02-23 03:56:13 +0000

    datatpes config

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,6 @@
+let age=21
+let name="gaurasnh"
+let add="@#$%"
+console.log(typeof age);
+console.log(typeof add);
+console.log(typeof name);
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId=4555
+let accountEmail = "kategauransh7@gmail.com"
+var phoneNumber = 8815383700
+accountCity="Burhanpur"
+
+accountPassword=8999908
+accountCity="jaipur"
+//prefer not to use var because of scope and functionality of scope
+console.log(accountId);
+console.table([accountEmail,accountCity,accountPassword])
+
+let accountstate;
+console.log(accountstate)
+
+let age=21
+console.log(typeof age);
+console.log(typeof accountEmail);
