6c3116f1f7aed9522e67c333dd7584b6a6f848b5
kategauransh <127082264+kategauransh@users.noreply.github.com>
2024-02-23 05:21:50 +0000

    conversion config

---
+++ 01_basics/01_conversion.js
@@ -0,0 +1,31 @@
+// let phoneNumber="gh"
+
+// console.log(typeof phoneNumber);
+// console.log(typeof (phoneNumber));
+
+// let phoneNumber2 = Number(phoneNumber)
+// console.log(typeof phoneNumber2);
+// console.log(phoneNumber2);
+
+
+// let phoneNumber1="null"
+
+// console.log(typeof phoneNumber1);
+// console.log(typeof (phoneNumber1));
+
+// let phoneNumber3 = Number(phoneNumber1)
+
+// console.log(phoneNumber3);
+// console.log(phoneNumber1);
+
+
+
+let isIn=1
+let loggedin=Boolean(isIn)
+
+console.log(loggedin);
+
+let ab =2
+let cd=String(ab)
+console.log(typeof cd);
+
